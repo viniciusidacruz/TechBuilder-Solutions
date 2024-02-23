9243fbe13f8df7d689d815f0da9696c351f6d6ff
viniciusidacruz <viniciusidacruz@hotmail.com>
2024-02-23 20:22:35 -0300

    feat(api): create settings for zod validation

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './services'
 import { CreateAccountController } from './controllers'
+import { envSchema } from './env'
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core'
+import { ConfigService } from '@nestjs/config'
+
+import { Env } from './env'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3333)
+  const configService: ConfigService<Env, true> = app.get(ConfigService)
+  const port = configService.get('PORT', { infer: true })
+
+  await app.listen(port)
 }
 bootstrap()
